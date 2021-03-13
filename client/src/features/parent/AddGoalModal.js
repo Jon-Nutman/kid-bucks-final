342e76dc6fbf2bf2d9659a922f41e8ba8cd35536
Jon-Nutman <jonnut730@gmail.com>
2021-03-13 12:16:50 -0500

    added add goal component

---
+++ client/src/features/parent/AddGoalModal.js
@@ -0,0 +1,22 @@
+import React , { useState } from 'react'
+import { Input, Form, InputNumber, Button } from 'antd';
+import SwitchablePicker from './SwitchablePicker'
+
+const { TextArea } = Input;
+
+
+export default function AddGoalModal() {
+    return (
+        <div>
+            <Form>
+                <Input placeholder='Add Goal Here'/>
+                <InputNumber placeholder='Points' />
+                <SwitchablePicker />
+                <TextArea rows={4} placeholder='description...' />
+                <Button type="primary" htmlType="submit">
+            Add Goal
+          </Button>
+            </Form>
+        </div>
+    )
+}
--- client/src/features/parent/ChildTab.js
+++ client/src/features/parent/ChildTab.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Tabs } from 'antd'
+import AddGoalModal from './AddGoalModal'
 
 const { TabPane } = Tabs;
 
@@ -11,7 +12,7 @@ export default function ChildTab() {
   return <div>
 <Tabs onChange={callback} type="card">
     <TabPane tab="Child 1" key="1">
-      Content of Tab Pane 1
+      <AddGoalModal/>
     </TabPane>
     <TabPane tab="Child 2" key="2">
       Content of Tab Pane 2
