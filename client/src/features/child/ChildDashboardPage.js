4136227b03a887ed264f85b42a4ebfc5589ea74b
Punchcode <punchcode@Punchcodes-MBP.fios-router.home>
2021-03-13 14:10:48 -0500

    goals list started

--- client/src/features/child/ChildDashboardPage.js
+++ client/src/features/child/ChildDashboardPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './Header'
+import GoalList from '../commonComponents/GoalList'
 
 export default function ChildDashboardPage() {
     return (
         <>
             <Header />
+            <GoalList />
         </>
     )
 }
