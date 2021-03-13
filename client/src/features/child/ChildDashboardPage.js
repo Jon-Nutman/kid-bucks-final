363c5bdff8570a8f706fce07ba447640a9f8094f
Punchcode <punchcode@Punchcodes-MBP.fios-router.home>
2021-03-13 11:42:22 -0500

    header and new file

--- client/src/features/child/ChildDashboardPage.js
+++ client/src/features/child/ChildDashboardPage.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from './Header'
 
 export default function ChildDashboardPage() {
     return (
-        <div>
-            children!
-        </div>
+        <>
+            <Header />
+        </>
     )
 }
