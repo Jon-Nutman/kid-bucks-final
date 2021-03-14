fddff6ab8a83a4865b8227b1707d5fa0009057ef
Jon-Nutman <jonnut730@gmail.com>
2021-03-14 19:02:42 -0400

    cleaned up weird indent and cleaned up code for all pages i thouched

--- client/src/features/parent/AddGoalModal.js
+++ client/src/features/parent/AddGoalModal.js
@@ -1,23 +1,22 @@
-import React , { useState } from 'react'
-import { Input, Form, InputNumber, Button } from 'antd';
-import SwitchablePicker from './SwitchablePicker'
+import React, { useState } from "react"
+import { Input, Form, InputNumber, Button } from "antd"
+import SwitchablePicker from "./SwitchablePicker"
 
-const { TextArea } = Input;
-
+const { TextArea } = Input
 
 export default function AddGoalModal() {
-    return (
-        <div>
-            <h1>Add Goal Modal</h1>
-            <Form>
-                <Input placeholder='Add Goal Here'/>
-                <InputNumber placeholder='Points' />
-                <SwitchablePicker />
-                <TextArea rows={4} placeholder='description...' />
-                <Button type="primary" htmlType="submit">
-            Add Goal
-          </Button>
-            </Form>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Add Goal Modal</h1>
+      <Form>
+        <Input placeholder="Add Goal Here" />
+        <InputNumber placeholder="Points" />
+        <SwitchablePicker />
+        <TextArea rows={4} placeholder="description..." />
+        <Button type="primary" htmlType="submit">
+          Add Goal
+        </Button>
+      </Form>
+    </div>
+  )
 }
--- client/src/features/parent/ParentDashboardPage.js
+++ client/src/features/parent/ParentDashboardPage.js
@@ -1,18 +1,18 @@
-import React from 'react'
+import React from "react"
 
-import AddChildModal from './AddChildModal'
-import ChildTab from './ChildTab'
-import { Tabs } from 'antd'
+import AddChildModal from "./AddChildModal"
+import ChildTab from "./ChildTab"
+import { Tabs } from "antd"
 import styles from "./ParentDashboard.module.css"
 export default function ParentDashboardPage() {
-    return (
-        <div>
-            <div className={styles.addChild}>
-            <AddChildModal />
-            </div>
-            <div>
-            <ChildTab />
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <div className={styles.addChild}>
+        <AddChildModal />
+      </div>
+      <div>
+        <ChildTab />
+      </div>
+    </div>
+  )
 }
