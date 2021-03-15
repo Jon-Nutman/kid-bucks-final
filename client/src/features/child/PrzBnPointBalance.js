fb0ae74691734bb124a817622c8b8ac4a5b89d76
Punchcode <punchcode@Punchcodes-MBP.fios-router.home>
2021-03-15 13:36:15 -0400

    the point total

---
+++ client/src/features/child/PrzBnPointBalance.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './PrzBnPointBalance.module.css'
+ 
+ export const PrzBnPointBalance = () => {
+     return (
+         <>
+             <h4>Point Balance</h4>
+             <h1 className={styles.balance}>50 pts</h1>
+         </>
+     )
+ }
