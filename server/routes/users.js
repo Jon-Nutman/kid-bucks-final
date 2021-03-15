a68f664220d12c5ad98a561a79381ed3f4b4da59
thomas1117 <thomasshannon117@yahoo.com>
2021-03-15 11:01:51 -0700

    resolve malformed dependencies

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import conn from '../db.js'
-console.log(conn)
+
 const router = express.Router()
 router.get('/users', async (request, response) => {
   const users = await conn.raw(`SELECT * FROM users;`)
