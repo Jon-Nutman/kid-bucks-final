fd9f28b45337e2581ba045fe20967648e4f34ac5
Jon-Nutman <jonnut730@gmail.com>
2021-03-15 13:26:34 -0400

    add goal is now a modal

--- client/src/features/parent/AddGoalModal.js
+++ client/src/features/parent/AddGoalModal.js
@@ -1,22 +1,62 @@
-import React, { useState } from "react"
-import { Input, Form, InputNumber, Button } from "antd"
+import React from "react"
+import Modal from "react-modal"
+import { Form, Input, InputNumber, Button } from "antd"
 import SwitchablePicker from "./SwitchablePicker"
 
 const { TextArea } = Input
 
-export default function AddGoalModal() {
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+}
+
+export default function ModalTest() {
+  
+  var subtitle
+  const [modalIsOpen, setIsOpen] = React.useState(false)
+  function openModal() {
+    setIsOpen(true)
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+  }
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+
   return (
     <div>
+      <Button onClick={openModal}>Add Goal +</Button>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+          <div>
       <h1>Add Goal Modal</h1>
       <Form>
         <Input placeholder="Add Goal Here" />
         <InputNumber placeholder="Points" />
         <SwitchablePicker />
         <TextArea rows={4} placeholder="description..." />
-        <Button type="primary" htmlType="submit">
+      </Form>
+    </div>
+        
+        <Button type type="primary" htmlType="submit" onClick={closeModal}>
           Add Goal
         </Button>
-      </Form>
+      </Modal>
     </div>
   )
 }
