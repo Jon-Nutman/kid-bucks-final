e261956273dd6f8951fb6aa4ea675391f81b9fa0
Jon-Nutman <jonnut730@gmail.com>
2021-03-15 14:20:47 -0400

    all modal for parent dsh added

--- client/src/features/parent/AddPrizeModal.js
+++ client/src/features/parent/AddPrizeModal.js
@@ -1,20 +1,60 @@
 import React from "react"
-import { Input, Form, InputNumber, Button } from "antd"
+import Modal from "react-modal"
+import { Form, Input, InputNumber, Button } from "antd"
+
 
 const { TextArea } = Input
 
-export default function AddPrizeModal() {
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+}
+
+export default function ModalTest() {
+  
+  var subtitle
+  const [modalIsOpen, setIsOpen] = React.useState(false)
+  function openModal() {
+    setIsOpen(true)
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+  }
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+
   return (
     <div>
-      <h1>Add Prize Modal</h1>
+      <Button onClick={openModal}>Add Prize +</Button>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+          <div>
+            <h1>Add Prize</h1>
       <Form>
         <Input placeholder="Add Prize Name Here" />
         <InputNumber placeholder="Points" />
         <TextArea rows={4} placeholder="description..." />
-        <Button type="primary" htmlType="submit">
+      </Form>
+    </div>
+        <Button type type="primary" htmlType="submit" onClick={closeModal}>
           Add Prize
         </Button>
-      </Form>
+      </Modal>
     </div>
   )
 }
