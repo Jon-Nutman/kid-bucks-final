d33cf21d7858bf2bd2682fc3cedfb19bbdd6dfba
Jon-Nutman <jonnut730@gmail.com>
2021-03-15 12:09:37 -0400

    add child modal working

--- client/src/features/parent/AddChildModal.js
+++ client/src/features/parent/AddChildModal.js
@@ -1,4 +1,6 @@
 import React from "react"
+import ReactDOM from "react-dom"
+import Modal from "react-modal"
 import { Form, Input, InputNumber, Button } from "antd"
 
 const layout = {
@@ -19,67 +21,110 @@ const validateMessages = {
     range: "${label} must be between ${min} and ${max}",
   },
 }
-export default function AddChildModal() {
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+}
+
+// Make sure to bind modal to your appElement (http://reactcommunity.org/react-modal/accessibility/)
+//   Modal.setAppElement('#yourAppElement')
+
+export default function ModalTest() {
   const onFinish = (values) => {
     console.log(values)
   }
+  var subtitle
+  const [modalIsOpen, setIsOpen] = React.useState(false)
+  function openModal() {
+    setIsOpen(true)
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+  }
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+
   return (
     <div>
-     <Button >
-            Add Child +
-          </Button>
-      <Form
-        // {...layout}
-        name="nest-messages"
-        onFinish={onFinish}
-        validateMessages={validateMessages}
+      <Button onClick={openModal}>Add Child +</Button>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
       >
-        <Form.Item
-          name={["user", "name"]}
-          label="Name"
-          rules={[
-            {
-              required: true,
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
+        <div>
+          <Form
+            // {...layout}
+            name="nest-messages"
+            onFinish={onFinish}
+            validateMessages={validateMessages}
+          >
+            <Form.Item
+              name={["user", "name"]}
+              label="Name"
+              rules={[
+                {
+                  required: true,
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
 
-        <Form.Item
-          label="Username"
-          name="username"
-          rules={[{ required: true, message: "Please input your username!" }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: "Please input your password!" }]}
-        >
-          <Input.Password />
-        </Form.Item>
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[
+                { required: true, message: "Please input your username!" },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                { required: true, message: "Please input your password!" },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
 
-        <Form.Item
-          name={["user", "age"]}
-          label="Age(optional)"
-          rules={[
-            {
-              type: "number",
-              min: 0,
-              max: 99,
-            },
-          ]}
-        >
-          <InputNumber />
-        </Form.Item>
-        <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
+            <Form.Item
+              name={["user", "age"]}
+              label="Age(optional)"
+              rules={[
+                {
+                  type: "number",
+                  min: 0,
+                  max: 99,
+                },
+              ]}
+            >
+              <InputNumber />
+            </Form.Item>
+            <Form.Item
+              wrapperCol={{ ...layout.wrapperCol, offset: 8 }}
+            ></Form.Item>
+          </Form>
+        </div>
+        <Button type type="primary" htmlType="submit" onClick={closeModal}>
+          Submit
+        </Button>
+      </Modal>
     </div>
   )
 }
