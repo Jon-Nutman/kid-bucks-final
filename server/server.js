61550c842b4ef25e730f52678f91ac8ab0d5b777
thomas1117 <thomasshannon117@yahoo.com>
2021-03-16 12:32:44 -0700

    PATCH: add user middleware

--- server/server.js
+++ server/server.js
@@ -2,17 +2,21 @@ import express from 'express'
 import './config.js'
 import userRoutes from './routes/users.js'
 import authRoutes from './routes/auth.js'
+import attachUser from './middleware/user.js'
 const app = express()
 const PORT = 3001
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+app.use(attachUser)
+
 app.use('/api', userRoutes)
 app.use('/api', authRoutes)
 
-// body, params, query
-// app.get('/users', (request, response) => {
-//   response.json([{ id: 1, name: 'john' }])
+// // body, params, query
+// app.get('/api/users', (request, response) => {
+//   console.log(request.user)
+//   response.json(request.user)
 // })
 
 app.listen(PORT, () => {
