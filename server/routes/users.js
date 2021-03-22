85e69829e977c55288c51358610e118562e922e8
Jon-Nutman <jonnut730@gmail.com>
2021-03-22 10:25:43 -0400

    not the mapping

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,33 +1,49 @@
-import express from 'express'
-import conn from '../db.js'
+import express from "express"
+import conn from "../db.js"
 
 const router = express.Router()
 
-router.get('/users', async (request, response) => {
+router.get("/users", async (request, response) => {
   const users = await conn.raw(`SELECT * FROM users;`)
   response.json(users.rows)
 })
-router.get('/test-user', async (request, response) => {
+router.get("/test-user", async (request, response) => {
   console.log(request.user)
   response.json(request.user)
 })
 
-router.delete('/users/child/:childId', async (request, response) => {
+router.delete("/users/child/:childId", async (request, response) => {
   const childId = req.params.childId
   await conn.raw(
     `
     DELETE FROM users 
-    WHERE child_id=?;
+    WHERE id=?;
     `,
     [childId]
   )
-  response.json({ message: 'child deleted' })
+  response.json({ message: "child deleted" })
 })
 
-// router.patch('/users/child/:childId', async (request, response) => {
-//   const childlId = req.params.childId
-//   response.json({ message: 'child updated' })
-// })
+router.get("/users/child/:id" ,async (request, response)  => {
+  const childId = req.params.id
+  await conn.raw(
+    `SELECT * FROM users
+    WHERE parent_id = ?;`
+    ,
+    [id]
+  )
+  response.json({ message: "Here are your children" })
 
+})
+router.patch('/users/child/:childId', async (request, response) => {
+  const childlId = req.params.childId
+  await conn.raw(
+    `SELECT * FROM users
+    WHERE parent_id = ?;`
+    ,
+    [id]
+  )
+  response.json({ message: 'child updated' })
+})
 
 export default router
