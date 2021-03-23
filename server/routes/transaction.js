9a88d9cffc718b7aa5e6bebc0b739e392a58705e
thomas1117 <thomasshannon117@yahoo.com>
2021-03-23 13:22:50 -0700

    add transaction row

--- server/routes/transaction.js
+++ server/routes/transaction.js
@@ -4,7 +4,12 @@ import conn from '../db.js'
 const router = express.Router()
 
 router.get('/transactions', async (request, response) => {
-  response.json({ message: 'here' })
+  const transactionSQL = `SELECT * FROM transactions
+INNER JOIN prizes
+ON prizes.id = transactions.prize_id
+WHERE user_id = ? AND transactions.status = 'requested';`
+  const transactions = await conn.raw(transactionSQL, [2])
+  response.json(transactions.rows)
 })
 
 router.patch('/transactions/:transactionId', async (request, response) => {
